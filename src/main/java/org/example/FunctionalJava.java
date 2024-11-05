80919dae08e779a6ced16431e811ce1ce3155d54
yee0802 <NovusAa12@proton.me>
2024-11-05 11:01:33 +0000

    feat: add indexOfAtSymbol Consumer

--- src/main/java/org/example/FunctionalJava.java
+++ src/main/java/org/example/FunctionalJava.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class FunctionalJava {
     public static void main(String[] args) {
 
     }
+
+    public Consumer<String[]> indexOfAtSymbol = strings -> {
+        Arrays.stream(strings).forEach(s -> System.out.println(s.indexOf("@")));
+    };
+
 }
