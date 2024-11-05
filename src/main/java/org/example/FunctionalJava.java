25881643c5b06cbf3eef56208fd26bc3444f512c
Victor Boleac <victor.boleac@icloud.com>
2024-11-05 11:58:41 +0000

    feat: add emailConsumer and getemailaddresses supplier

--- src/main/java/org/example/FunctionalJava.java
+++ src/main/java/org/example/FunctionalJava.java
@@ -2,13 +2,14 @@ package org.example;
 
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class FunctionalJava {
     public static void main(String[] args) {
-
+        emailConsumer.accept(getEmailAddresses.get());
     }
 
     public Consumer<String[]> indexOfAtSymbol = strings -> Arrays.stream(strings).forEach(s -> System.out.println(s.indexOf("@")));
@@ -31,4 +32,13 @@ public class FunctionalJava {
     public BiConsumer<String, Integer> printsAgeAndName  = (name, age)->{
         System.out.println(STR."Hello \{name}!Hope you are well in your \{age}'s");
     };
+    public static Supplier<List<String>> getEmailAddresses = () -> Arrays.asList(
+            "simon.morgan@northcoders.com",
+            "chris.ward@northcoders.com",
+            "info@stackoverflow.com",
+            "hannah.montanna@mileycyrus.com");
+
+
+    public static Consumer<List<String>> emailConsumer = emails -> emails.forEach(System.out::println);
+
 }
