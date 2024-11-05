819082557f5e5f02dcdfb569decd61c1f9828e5a
yee0802 <NovusAa12@proton.me>
2024-11-05 11:21:09 +0000

    feat: add emailAddresses Supplier

--- src/main/java/org/example/FunctionalJava.java
+++ src/main/java/org/example/FunctionalJava.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class FunctionalJava {
     public static void main(String[] args) {
@@ -11,5 +12,12 @@ public class FunctionalJava {
     public Consumer<String[]> indexOfAtSymbol = strings -> {
         Arrays.stream(strings).forEach(s -> System.out.println(s.indexOf("@")));
     };
+
+    public Supplier<String[]> emailAddresses = () -> new String[]{
+                "simon.morgan@northcoders.com",
+                "chris.ward@northcoders.com",
+                "info@stackoverflow.com",
+                "hannah.montanna@mileycyrus.com"
+        };
 
 }
