93e79a7ee171ec9c0886a2fa07ebd1a8811709fd
Victor Boleac <victor.boleac@icloud.com>
2024-11-05 16:08:50 +0000

    feat: task11

--- src/main/java/org/example/TicketBooking.java
+++ src/main/java/org/example/TicketBooking.java
@@ -11,11 +11,17 @@ public class TicketBooking {
     //TO BE UPDATED:
     // handle the cases when ticket availability is insufficient
     public void updateTickets(TicketBooking booking){
-        int availability = event.getTicketAvailability() - booking.numberOfTickets;
-        event.setTicketAvailability(availability);
-
+        if(event.getTicketAvailability()>=booking.numberOfTickets) {
+            int availability = event.getTicketAvailability() - booking.numberOfTickets;
+            event.setTicketAvailability(availability);
+        }else{
+            // done
+            System.out.println(STR."There are only \{event.getTicketAvailability()} tickets available");
+        }
     }
     public double getTotalPrice(){
         return numberOfTickets* event.getTicketPrice();
     }
 }
+// Extend the system to allow for booking specific seats at each event.
+// hashMaps can be used kind of seat number (int) as K and availability (boolean) as V
