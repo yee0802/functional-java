0d3011ee960c743fad2707e858c0c2216e1595f4
yee0802 <NovusAa12@proton.me>
2024-11-05 14:17:17 +0000

    feat: add filterEmailAddresses BiFunction with example Predicates

--- src/main/java/org/example/FunctionalJava.java
+++ src/main/java/org/example/FunctionalJava.java
@@ -1,17 +1,14 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class FunctionalJava {
     public static void main(String[] args) {
         FunctionalJava functionalJava = new FunctionalJava();
 
-        // functionalJava.printEmailsInList.accept(functionalJava.getEmailAddresses.get());
+        /* functionalJava.printEmailsInList.accept(functionalJava.getEmailAddresses.get());
 
         List<Integer> numList = new ArrayList<>();
         numList.add(5);
@@ -21,6 +18,16 @@ public class FunctionalJava {
         numList.add(67);
 
         functionalJava.squareNumbers.andThen(functionalJava.printNumbersInList).accept(numList);
+        */
+
+        var filteredEmails1 = functionalJava.filterEmailAddresses.apply(functionalJava.emailAddressSupplier, isValidCommercialEmail);
+        System.out.println(filteredEmails1);
+
+        var filteredEmails2 = functionalJava.filterEmailAddresses.apply(functionalJava.emailAddressSupplier, isValidBritishEmail);
+        System.out.println(filteredEmails2);
+
+        var filteredEmails3 = functionalJava.filterEmailAddresses.apply(functionalJava.emailAddressSupplier, Predicate.not(isValidCommercialEmail.or(isValidBritishEmail)));
+        System.out.println(filteredEmails3);
     }
 
     public Consumer<String[]> indexOfAtSymbol = strings -> Arrays.stream(strings).forEach(s -> System.out.println(s.indexOf("@")));
@@ -59,5 +66,28 @@ public class FunctionalJava {
 
     public BiConsumer<List<Integer>, List<Integer>> compareListOfIntegers = (nums1, nums2) -> {
             System.out.println(nums1.equals(nums2));
+    };
+
+    public Supplier<List<String>> emailAddressSupplier = () -> Arrays.asList(
+            "simon.morgan@northcoders.com",
+            "chris.ward@northcoders.com",
+            "mario@plumbing.it",
+            "link@hyrule.co.uk",
+            "shrek@duloc.com",
+            "ellie@northcoders.js",
+            "java@oracle.jvm",
+            "ziggy@spidersfrommars.co.uk",
+            "lemmy@motorhead.co,uk",
+            "me@myhouse.sleep"
+            );
+
+    public static Predicate<String> isValidCommercialEmail = s -> s.endsWith(".com");
+
+    public static Predicate<String> isValidBritishEmail = s -> s.endsWith(".co.uk");
+
+    public BiFunction<Supplier<List<String>>, Predicate<String>, List<String>> filterEmailAddresses = (listSupplier, stringPredicate) -> {
+        List<String> filteredEmails = listSupplier.get().stream().filter(stringPredicate).toList();
+
+        return filteredEmails;
     };
 }
